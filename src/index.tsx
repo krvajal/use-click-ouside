d84bd17e7a6f9812a4596dcff16c67267a5fa901
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2019-10-12 19:21:43 +0200

    feat: add implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+export type Config = {
+  skip?: boolean
+  onClickOutside: (evt: MouseEvent) => any
+}
+
+export default function useClickOutside(
+  ref: HTMLElement | null,
+  { skip = false, onClickOutside }: Config,
+  deps: any[] = []
+) {
+  React.useEffect(() => {
+    if (!skip) {
+      document.addEventListener("click", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("click", handleClickOutside);
+      };
+    }
+    function handleClickOutside(evt: MouseEvent) {
+      if (ref && !ref.contains(evt.target as Node)) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        onClickOutside(evt);
+      }
+    }
+  }, [...deps, onClickOutside, skip]);
+}
